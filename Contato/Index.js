83689b024b35206843de1f01ca93d261be3be509
TiagoLuizSS <tgluizs@gmail.com>
2022-08-21 21:10:39 -0300

    Outros detales

--- Contato/Index.js
+++ Contato/Index.js
@@ -3,10 +3,10 @@ import readlineSync from 'readline-sync';
 
 
 
-    const nomeCliente = readlineSync.question(chalk.underline("digite seu nome ou CPF: "));
-
-            const respostas = readlineSync.question("Bom te ver denovo " + chalk.green(nomeCliente) 
-            + ", em que posso ajudar ?: \n\n" 
+        const nomeCliente = readlineSync.question(chalk.underline("\ndigite seu nome: "));
+            const respostas = readlineSync.question("Bom te ver denovo "           
+                + chalk.green(nomeCliente) 
+                + ", em que posso ajudar ?: \n\n"
             + chalk.rgb(255, 136, 0).bold("1") + ". Fatura \n" 
             + chalk.rgb(255, 136, 0).bold("2") + ". Caiu um meteoro na minha rua e destruiu a rede \n" 
             + chalk.rgb(255, 136, 0).bold("3") + ". Conexao ta uma uva \n"
@@ -14,11 +14,11 @@ import readlineSync from 'readline-sync';
 
                 function resultado(numero){
                     if(numero === "1"){
-                        return "\nFinalmente hein" + chalk.green(nomeCliente)
+                        return "\nFinalmente hein " + chalk.green(nomeCliente)
                         + ", Aqui esta sua fatura:\n\n\n\n\n IMAGEM PDF kk.\n\n\n\n\n";
                     }
                     if(numero === "2"){
-                        return "\nVish...," + chalk.green(nomeCliente) 
+                        return "\nVish..., " + chalk.green(nomeCliente) 
                         + " Aguarde que ligaremos para agendar uma visita\n"
                     }
                     if(numero === "3"){
